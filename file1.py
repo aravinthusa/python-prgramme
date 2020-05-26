5bfcabd6aae1b262e9dc2f67fd243676938e475d
hariharan0508 <65960054+hariharan0508@users.noreply.github.com>
2020-05-26 19:43:33 +0530

    created file.py
    programmes

---
+++ file1.py
@@ -0,0 +1,25 @@
+program:
+
+#to find the radius of the circle
+
+r=float(input("input the radius of the circle:"))
+a=3.14*r*r
+print("the area of circle with radius",r,"is:",a)
+
+output:
+
+input the radius of the circle:1.1
+the area of circle with radius 1.1 is: 3.7994000000000008
+
+program:
+
+#to find the extension of the file
+
+a=input("Input the Filename: ")
+b=a.split(".")
+print ("The extension of the file is : " ,b[-1])
+
+output:
+
+Input the Filename: abc.py
+The extension of the file is :  py
