019fdf47128ed1ebac447f3b87b23938b21d50a5
hariharan0508 <65960054+hariharan0508@users.noreply.github.com>
2020-05-26 20:48:40 +0530

    created file2.py

---
+++ file2.py
@@ -0,0 +1,70 @@
+program:
+
+#fibonaccci series
+
+n=int(input("enter the number of sequence:"))
+a=0
+b=1
+i=0
+print(a)
+print(b)
+while(i<n):
+ c=a+b
+ print(c)
+ a=b
+ b=c
+ i=i+1
+ 
+ output:
+ 
+ enter the number of sequence:15
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+987
+
+program:
+
+#to print all positive numbers in a range
+
+list1=[12,-7,5,64,-14]
+print("input:",list1) 
+for num in list1: 
+    if num >= 0:      
+       print("output:",num, end = " ")
+output:
+
+input: [12, -7, 5, 64, -14]
+output: 12 output: 5 output: 64 
+
+program:
+
+#to print all positive numbers in a range
+
+list1=[12,14,-95,3]
+print("input:",list1) 
+for num in list1: 
+    if num >= 0:      
+       print("output:",num, end = " ")
+       
+output:
+
+   input: [12, 14, -95, 3]
+   output: 12 output: 14 output: 3
+       
+       
+       
+ 
